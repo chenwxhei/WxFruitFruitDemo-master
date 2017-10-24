e10646b68856bfced477124253189a50f6fa0656
13766 <13766@>
2017-10-24 13:09:51 +0800

    商品详情，用户

---
+++ WeFruit.Weixin/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeFruit.IService;
+using WeFruit.Weixin.Models;
+
+namespace WeFruit.Weixin.Controllers
+{
+    public class ProductController : Controller
+    {
+        HomeViewModel homeViewModel = new HomeViewModel();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ISortService SortService { get; set; }
+        public ActionResult Classify()
+        {
+            homeViewModel.Sorts = SortService.GetEntities(b => true);
+            return View(homeViewModel);
+        }
+
+        public IProductService ProductService { get; set; }
+
+        public ActionResult Commodity(int code)
+        {
+            homeViewModel.Products = ProductService.GetEntities(b => b.Sorts.First().Code == code);
+            return View(homeViewModel);
+        }
+    }
+}
